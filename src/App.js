7a33ba7179e9ba774f45fd18d7dc2dadb3eef089
Felipe Lopes <felipefllp@msn.com>
2021-07-30 21:06:36 -0300

    feat: utilização do date-fns para cálculo de tempo de vida

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import Header from './components/Header'
 import Main from './components/Main';
 import DateInput from './components/DateInput';
 
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInWeeks,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+} from 'date-fns'
+
 export default function App() {
-  
-  const [Date, setDate] = useState('1988-03-28');
+
+  const [date, setDate] = useState('');
+
+  const [years, setYears] = useState();
+  const [months, setMonths] = useState();
+  const [weeks, setWeeks] = useState();
+  const [days, setDays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
 
   function handleDateChange(newDate) {
     setDate(newDate)
+
+    const today = new Date()
+    const birthday = new Date(newDate)
+
+    setYears(differenceInYears(today, birthday))
+    setMonths(differenceInMonths(today, birthday))
+    setWeeks(differenceInWeeks(today, birthday))
+    setDays(differenceInDays(today, birthday))
+    setHours(differenceInHours(today, birthday))
+    setMinutes(differenceInMinutes(today, birthday))
+    setSeconds(differenceInSeconds(today, birthday))
   }
 
   return (
@@ -16,14 +46,22 @@ export default function App() {
       <Header>Trabalho Prático</Header>
       <Main>
         <DateInput
-          labelDescription="Informe sua data de nascimento:"
-          inputValue={Date}
+          labelDescription="Informe sua data e horario de nascimento:"
+          inputValue={date}
           onInputChange={handleDateChange}
         />
-
-        <p className="text-center">
-          Asd
-        </p>
+        {date ?
+          <div className="flex flex-col items-center">
+            <div>
+              <p> {`Você está vivo há ${years.toLocaleString()} anos.`} </p>
+              <p> {`Ou ${months.toLocaleString()} meses.`} </p>
+              <p> {`Ou ${weeks.toLocaleString()} semanas.`} </p>
+              <p> {`Ou ${days.toLocaleString()} dias.`} </p>
+              <p> {`Ou ${hours.toLocaleString()} horas.`} </p>
+              <p> {`Ou ${minutes.toLocaleString()} minutos.`} </p>
+              <p> {`Ou ${seconds.toLocaleString()} segundos.`} </p>
+            </div>
+          </div> : ''}
       </Main>
     </>
   );
